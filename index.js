05b5b320eacc1d201ed28262dc5e320dbf7cc4f2
AlejandroPolskoy <mitchel_nk@yahoo.es>
2023-05-20 15:43:32 +0200

    vercel test3

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use((req, res , next) => {
     res.header('Access-Control-Allow-Method', 'POST, GET, DELETE, PUT, PATCH');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://proyecto-final-maletas.vercel.app/');
     next();
 })
 
